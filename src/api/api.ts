e255ffd135bc93d30726bcd8dd92027f100f3710
Randallflagg19 <121073882+Randallflagg19@users.noreply.github.com>
2024-12-02 13:49:01 +0300

    chore: cors problem fx

--- src/api/api.ts
+++ src/api/api.ts
@@ -11,7 +11,8 @@ export enum ResultCodeForCaptcha {
 
 export const axiosInstance = axios.create({
 	withCredentials: true,
-	baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+	baseURL: 'https://gustaw.ru/remoteapi/',
+	// baseURL: 'https://social-network.samuraijs.com/api/1.0/',
 	headers: {
 		'API-KEY': 'fc0fedd9-f046-4e76-b27c-fe00a05918eb'
 	}
